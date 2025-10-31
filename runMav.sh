f1231653d839fdc9aac491944921853dff7f977c
Kahjiitspy <artyrhamuliak@gmail.com>
2025-10-31 12:32:53 -0400

    Crow fixes

--- runMav.sh
+++ runMav.sh
@@ -1,3 +1,9 @@
-cd ~/Documents/Projects/Raven/Raven || exit
-mvn package
-java -cp target/feather.jar Graveyard.App
+cd ~/Documents/Projects/Raven/Crow || exit
+# mvn package
+# java -cp target/caw.jar Graveyard.App
+mvn clean package
+java -jar target/caw-jar-with-dependencies.jar
+
+# cd ~/Documents/Projects/Raven/Raven || exit
+# mvn package
+# java -cp target/feather.jar Graveyard.App
